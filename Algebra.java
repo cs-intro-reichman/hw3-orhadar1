653bc452ec6bdbb4c97400abeb565277cbd651d4
orhadar1 <or.hadar01@post.runi.ac.il>
2025-11-15 00:40:50 +0200

    finish?

--- Algebra.java
+++ Algebra.java
@@ -25,43 +25,88 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int i = 0;
+		int resultplus = 0;
+		while(i < x1) {
+			x2++;
+			i++;
+		}
+		resultplus = x2;
+		return resultplus;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int resultminus = 0;
+		while(x2 != 0) {
+			x2--;
+			x1--;
+		}
+		resultminus = x2;
+		return resultminus;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int resulttimes = 0;
+		int tempx1 = x1;
+		while(x2 != 1){
+			x1 = plus(tempx1, x1);
+			x2--;
+		}
+		x1 = resulttimes;
+		return resulttimes;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		int tempx = 0;
+		int resultpow = 0;
+		while(n != 1){
+			x = times(tempx,x);
+			n--;
+		}
+		resultpow = x;
+		return resultpow;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int resultdiv = 0;
+		while (x1 != 0) {
+			x1 = minus(x1, x2);
+			resultdiv++;
+		}
+		return resultdiv;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int timesofmod = 0;
+		int resltmod = 0;
+		int tempx2 = x2;
+		int tempx1 = x1;
+		while(x1 > x2 && x2 != 0){
+			x1 = minus(x1, x2);
+			x2--;
+			timesofmod++;
+		}
+		int resulttimes = times(timesofmod, tempx2);
+		resltmod = minus(tempx1, resulttimes);
+		return resltmod;
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
+	int i = 1;
+	int resultsqrt = 0;
+	while (pow(i, i) != x) {
+		i++;
+
+	}
+	i++;
+	resultsqrt = pow(i, i);
+		return resultsqrt;
 	}	  	  
 }
+
