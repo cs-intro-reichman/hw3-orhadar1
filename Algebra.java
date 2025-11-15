1c60aaba0fa2126efec66209826b569d476faa78
orhadar1 <or.hadar01@post.runi.ac.il>
2025-11-15 18:54:59 +0200

    finish task 1

--- Algebra.java
+++ Algebra.java
@@ -6,107 +6,157 @@
 public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
-   		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
-   		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-   		System.out.println(pow(5,3));      // 5^3
-   		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
-   		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
-   		System.out.println(mod(25,7));   // 25 % 7
-   		System.out.println(mod(120,6));  // 120 % 6    
-   		System.out.println(sqrt(36));
-		System.out.println(sqrt(263169));
-   		System.out.println(sqrt(76123));
+	    //System.out.println(plus(2,-3));   // 2 + 3
+	    //System.out.println(minus(-7,2));  // 7 - 2
+   		//System.out.println(minus(2,7));  // 2 - 7
+ 		//System.out.println(times(10,2));  // 3 * 4
+   		//System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
+   		//System.out.println(pow(1,2));      // 5^3
+   		//System.out.println(pow(3,2));      // 3^5
+   		//System.out.println(div(12,3));   // 12 / 3    
+   		//System.out.println(div(10,2));    // 5 / 5  
+   		//System.out.println(div(21,-7));   // 25 / 7
+   		//System.out.println(mod(25,7));   // 25 % 7
+   		//System.out.println(mod(7,5));  // 120 % 6    
+   		//System.out.println(sqrt(4));
+		//System.out.println(sqrt(263169));
+   		//System.out.println(sqrt(76123));
 	}  
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		int i = 0;
-		int resultplus = 0;
-		while(i < x1) {
-			x2++;
-			i++;
-		}
-		resultplus = x2;
-		return resultplus;
+	 if (x2 > 0) {
+        while (x2 != 0) {
+            x1++;   
+            x2--;   
+        }
+    } else {
+        while (x2 != 0) {
+            x1--;  
+            x2++;   
+        }
+    }
+    return x1;
+
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		int resultminus = 0;
-		while(x2 != 0) {
-			x2--;
-			x1--;
-		}
-		resultminus = x2;
-		return resultminus;
+		 if (x2 > 0) {
+        while (x2 != 0) {
+            x1--;   
+            x2--;   
+        }
+    } else {
+        while (x2 != 0) {
+            x1++;  
+            x2++;   
+        }
+    }
+    return x1;
+
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		int resulttimes = 0;
-		int tempx1 = x1;
-		while(x2 != 1){
-			x1 = plus(tempx1, x1);
-			x2--;
-		}
-		x1 = resulttimes;
-		return resulttimes;
+		int tempx1 = Math.abs(x1);
+		int tempx2 = Math.abs(x2);
+		if (x2 == 0 || x1 == 0) {
+				return 0;
+			}
+			if(x1 < 0 && x2 < 0){
+				tempx2++;
+				while(tempx2 >= 1)
+				{
+					x1 = plus(x1, tempx1);
+					tempx2--;
+				}
+			}
+			else if(x2 < 0 && tempx2 > 0 && x1 > 0){
+				while(tempx2 >= 0)
+				{
+					x1 = minus(x1,tempx1);
+					tempx2--;
+				}
+			}
+			else if(x1 < 0 && tempx2 > 0 && x2 > 0)
+			{
+				while(tempx2 > 1)
+				{
+					x1 = minus(x1, tempx1);
+					tempx2--;
+				}
+			}
+			else{
+				while(tempx2 != 1){
+					x1 = plus(x1, tempx1);
+					tempx2--;
+				}
+			}
+		return x1;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		int tempx = 0;
-		int resultpow = 0;
+		int tempx = x;
+		
 		while(n != 1){
 			x = times(tempx,x);
 			n--;
 		}
-		resultpow = x;
-		return resultpow;
+		
+		return x;
 	}
 
-	// Returns the integer part of x1 / x2 
+	
 	public static int div(int x1, int x2) {
-		int resultdiv = 0;
-		while (x1 != 0) {
-			x1 = minus(x1, x2);
-			resultdiv++;
-		}
-		return resultdiv;
+		boolean negative = false;
+
+  
+    if (x1 < 0) {
+        negative = !negative;     
+        x1 = minus(0, x1);        
+    }
+
+    if (x2 < 0) {
+        negative = !negative;
+        x2 = minus(0, x2);         
+    }
+
+    
+    int result = 0;
+    int remaining = x1;
+
+    // כמה פעמים x2 נכנס בתוך x1?
+    while (remaining >= x2) {
+        remaining = minus(remaining, x2);  
+        result   = plus(result, 1);        
+    }
+
+
+    if (negative && result != 0) {
+        result = minus(0, result);        
+    }
+
+    return result;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		int timesofmod = 0;
-		int resltmod = 0;
-		int tempx2 = x2;
-		int tempx1 = x1;
-		while(x1 > x2 && x2 != 0){
-			x1 = minus(x1, x2);
-			x2--;
-			timesofmod++;
-		}
-		int resulttimes = times(timesofmod, tempx2);
-		resltmod = minus(tempx1, resulttimes);
-		return resltmod;
+		
+		 while (x1 >= x2) {
+        x1 = minus(x1, x2);  
+    }
+		return x1;
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
 	int i = 1;
-	int resultsqrt = 0;
-	while (pow(i, i) != x) {
+	while (times(i, i) != x) {
 		i++;
-
 	}
-	i++;
-	resultsqrt = pow(i, i);
-		return resultsqrt;
+		return i;
 	}	  	  
 }
 
